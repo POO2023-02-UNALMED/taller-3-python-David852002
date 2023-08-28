b231efedfd628bb33c43a218c66f28d930ce4b93
David852002 <ddelgadoo@unal.edu.co>
2023-08-28 18:23:21 -0500

    f

---
+++ televisores/control.py
@@ -0,0 +1,42 @@
+class Control:
+    def enlazar(self, tv):
+        self.tv = tv
+        tv.setControl(self)
+        
+    def turnOn(self):
+        if self.tv:
+            self.tv.turnOn()
+        
+    def turnOff(self):
+        if self.tv:
+            self.tv.turnOff()
+            
+    def canalUp(self):
+        if self.tv:
+            self.tv.canalUp()
+            
+    def canalDown(self):
+        if self.tv:
+            self.tv.canalDown()
+            
+    def volumenUp(self):
+        if self.tv:
+            self.tv.volumenUp()
+            
+    def volumenDown(self):
+        if self.tv:
+            self.tv.volumenDown()
+            
+    def setCanal(self, canal):
+        if self.tv:
+            self.tv.setCanal(canal)
+            
+    def setVolumen(self, volumen):
+        if self.tv:
+            self.tv.setVolumen(volumen)
+            
+    def setTv(self, tv):
+        self.tv = tv
+        
+    def getTv(self):
+        return self.tv
